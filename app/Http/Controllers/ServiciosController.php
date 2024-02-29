785de94a4b78040b5346654fdc36312dc79313e2
Tonho1994 <tono.morazu.94@gmail.com>
2024-02-28 20:44:06 -0600

    ajuste a llaves vacias en servicios controller

--- app/Http/Controllers/ServiciosController.php
+++ app/Http/Controllers/ServiciosController.php
@@ -36,8 +36,41 @@ class ServiciosController extends Controller
                 ->get());
         }
         else{
-            return new ProductoServicioCollection(ProductoServicio::ServicioHijos($validator->validated()['id'])
-                ->get());
+            //primera solucion de filtrado
+            $servicios = self::filtraArray(ProductoServicio::ServicioHijos($validator->validated()['id'])->get()->toArray());
+            return response(['servicios'=>$servicios], 200);
+            //sin filtrado
+            /* return new ProductoServicioCollection(ProductoServicio::ServicioHijos($validator->validated()['id'])
+                ->get()); */
         }
+    }
+    /**
+     * Filtra un arreglo de servicios
+     *
+     * @param  array  $datos array de servicios
+     * @return array El mismo array sin llaves vacias
+     */
+    private static function filtraArray(array $datos){
+        foreach($datos as $key => $servicio) {
+            if(empty($servicio['materiales'])) {
+                unset($datos["$key"]['materiales']);
+            }
+            if(empty($servicio['tipos_vehiculos'])) {
+                unset($datos["$key"]['tipos_vehiculos']);
+            }
+            if(empty($servicio['tamanos'])) {
+                unset($datos["$key"]['tamanos']);
+            }
+            if(empty($servicio['tipos_colchones'])) {
+                unset($datos["$key"]['tipos_colchones']);
+            }
+            if(empty($servicio['tipos_pelajes'])) {
+                unset($datos["$key"]['tipos_pelajes']);
+            }
+            if(empty($servicio['padre_producto_servicio_id'])) {
+                unset($datos["$key"]['padre_producto_servicio_id']);
+            }
+        }
+        return $datos;
     }
 }
